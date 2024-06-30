11ebfc2b49b92c806d469fc9a76bbecc8540f158
John Lundgren <john.lund.af@gmail.com>
2024-06-30 08:46:09 +0200

    Added form component.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,56 @@
+import { FormProps } from '../types/component.types';
+import { useState, ChangeEvent } from 'react';
 
-export default function Form() {
+interface FormData {
+  initialInvestment: string,
+  annualInvestment: string,
+  expectedReturn: string,
+  duration: string
+}
+
+
+export default function Form({ onInvestmentChange }: FormProps) {
+
+  const [formData, setFormData] = useState<FormData>({
+    initialInvestment: "",
+    annualInvestment: "",
+    expectedReturn: "",
+    duration: ""
+  });
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    const propName = event.target.name;
+    setFormData(current => ({ ...current, [propName]: value }))
+  }
+
+  const allInputsContainNumbers = Object.values(formData).every(value => value && !isNaN(value));
+
+  if (allInputsContainNumbers) {
+    onInvestmentChange({
+      initialInvestment: +formData.initialInvestment,
+      annualInvestment: +formData.annualInvestment,
+      expectedReturn: +formData.expectedReturn,
+      duration: +formData.duration
+    });
+  }
+
   return <form id="form">
     <div className="form-group">
       <label htmlFor="initial">Initial Investment</label>
-      <input type="number" name="initial" id="initial" />
+      <input value={formData.initialInvestment} onChange={handleChange} type="number" name="initialInvestment" id="initial" />
     </div>
     <div className="form-group">
       <label htmlFor="annual">Annual Investment</label>
-      <input type="number" name="annual" id="annual" />
+      <input value={formData.annualInvestment} onChange={handleChange} type="number" name="annualInvestment" id="annual" />
     </div>
     <div className="form-group">
       <label htmlFor="expected">Expected Return</label>
-      <input type="number" name="expected" id="expected" />
+      <input value={formData.expectedReturn} onChange={handleChange} type="number" name="expectedReturn" id="expected" />
     </div>
     <div className="form-group">
       <label htmlFor="duration">Duration</label>
-      <input type="number" name="duration" id="duration" />
+      <input value={formData.duration} onChange={handleChange} type="number" name="duration" id="duration" />
     </div>
   </form>
 }
