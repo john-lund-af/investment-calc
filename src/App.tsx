3fcdf002677edfd3b8a22f4d40330a2fdbdabeb1
John Lundgren <john.lund.af@gmail.com>
2024-07-01 09:15:34 +0200

    Adding useState for investment value.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import Header from './components/Header';
 import Form from './components/Form';
 import ResultTable from './components/ResultTable';
+import { useState } from 'react';
+import { Investment } from './types/common.types';
 
 function App() {
+
+  const [investment, setInvestment] = useState<Investment>({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    duration: 0,
+    expectedReturn: 0
+  });
+
+  const allValuesFilled = Object.values(investment).every(value => value && !isNaN(value));
 
   return (
     <main>
       <Header />
-      <Form />
-      <ResultTable />
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <Form investment={investment} onInvestmentChange={setInvestment} />
+      {allValuesFilled && <ResultTable investment={investment} />}
     </main>
   )
 }
