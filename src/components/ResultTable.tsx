4e5f480707452ce6e1a0784b6c1d0382afe6c85e
John Lundgren <john.lund.af@gmail.com>
2024-07-01 09:15:57 +0200

    Added component for result.

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,6 +1,12 @@
+import { ResultTableProps } from '../types/component.types';
+import { calculateInvestmentResults, formatter } from '../util/investment';
 
-export default function ResultTable() {
-  return <table id="result">
+export default function ResultTable({ investment }: ResultTableProps) {
+
+  const investmentResults = calculateInvestmentResults(investment);
+  const initialInvestment = investmentResults[0].valueEndOfYear - investmentResults[0].interest - investmentResults[0].annualInvestment;
+
+  return <table id="result" >
     <thead>
       <tr>
         <th>Year</th>
@@ -10,5 +16,19 @@ export default function ResultTable() {
         <th>Invested Capital</th>
       </tr>
     </thead>
+    <tbody>
+      {investmentResults.map(result => {
+        const totalInterest = result.valueEndOfYear - result.annualInvestment * result.year - initialInvestment;
+        const totalAmountInvested = result.valueEndOfYear - totalInterest;
+
+        return <tr key={result.year}>
+          <td>{result.year}</td>
+          <td>{formatter.format(result.valueEndOfYear)}</td>
+          <td>{formatter.format(result.interest)}</td>
+          <td>{formatter.format(totalInterest)}</td>
+          <td>{formatter.format(totalAmountInvested)}</td>
+        </tr>
+      })}
+    </tbody>
   </table>
 }
